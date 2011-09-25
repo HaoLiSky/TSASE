97e143b72725472d8742ed4ae56c48ef7c802ad5
xph <xph@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2011-09-25 23:25:17 +0000

    Global version: velocity and force are global vectors containing all images information

--- neb/fire_ssneb.py
+++ neb/fire_ssneb.py
@@ -35,8 +35,8 @@ class fire_ssneb(minimizer_ssneb):
         self.Nsteps=0
 
         #self.v contains the velocity for both atoms and box for one image
-	i = self.band.numImages-2
-	j = self.band.natom+3
+    	i = self.band.numImages-2
+    	j = self.band.natom+3
         self.v = np.zeros((i,j,3))
 	
         
@@ -45,36 +45,41 @@ class fire_ssneb(minimizer_ssneb):
         Fire step
         """
         self.band.forces()
+        totalf = self.v.copy()
         for i in range(1, self.band.numImages - 1):
-	        totalf = self.band.path[i].totalf
-		Power = vdot(totalf,self.v[i-1])
-		if Power > 0.0:
-		    self.v[i-1] = (1.0-self.a)*self.v[i-1]+self.a*vmag(self.v[i-1])*vunit(totalf)
-		    if(self.Nsteps>self.Nmin):
-			self.dt = min(self.dt*self.finc,self.dtmax)
-			self.a *= self.fa
-		    self.Nsteps+=1
-		else:
-		    # reset velocity and slow down
-		    self.v[i-1] *= 0.0
-		    self.a   = self.astart
-		    self.dt *= self.fdec
-		    self.Nsteps  = 0
-		# Euler step
-		self.v[i-1] += self.dt * totalf
-		# check for max step
-                if vmag(self.v[i-1]) > self.maxmove/self.dt :
-                    self.v[i-1] = self.maxmove/self.dt * vunit(self.v[i-1])
-                dR = self.dt * self.v[i-1]
-		# move R 
-                rt  = self.band.path[i].get_positions()
-		rt += dR[:-3]
-                self.band.path[i].set_positions(rt)
+	        totalf[i-1] = self.band.path[i].totalf
+	Power = vdot(totalf,self.v)
 
-		# move box and update cartesian coordinates 
-                ct  = self.band.path[i].get_cell()
-		ct += np.dot(ct, dR[-3:]) / self.band.jacobian
-                self.band.path[i].set_cell(ct, scale_atoms=True)
+        if Power > 0.0:
+            self.v = (1.0-self.a)*self.v + self.a*vmag(self.v)*vunit(totalf)
+            if(self.Nsteps>self.Nmin):
+                self.dt = min(self.dt*self.finc,self.dtmax)
+                self.a *= self.fa
+            self.Nsteps+=1
+        else:
+            # reset velocity and slow down
+            self.v  *= 0.0
+            self.a   = self.astart
+            self.dt *= self.fdec
+            self.Nsteps  = 0
+
+        # Euler step
+        self.v += self.dt * totalf
+	# check for max step
+        if vmag(self.v[i-1]) > self.maxmove/self.dt :
+            self.v[i-1] = self.maxmove/self.dt * vunit(self.v[i-1])
+
+        for i in range(1, self.band.numImages - 1):
+            dR = self.dt * self.v[i-1]
+            # move R 
+            rt  = self.band.path[i].get_positions()
+            rt += dR[:-3]
+            self.band.path[i].set_positions(rt)
+
+            # move box and update cartesian coordinates 
+            ct  = self.band.path[i].get_cell()
+            ct += np.dot(ct, dR[-3:]) / self.band.jacobian
+            self.band.path[i].set_cell(ct, scale_atoms=True)
 
         
         
