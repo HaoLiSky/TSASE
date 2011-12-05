d929f50db70b8b2bbbd8a2b8ea8889df9d5cede3
rye <rye@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2011-12-05 22:40:17 +0000

    cli tools

--- bin/tutil.py
+++ bin/tutil.py
@@ -2,8 +2,18 @@
 import sys
 import pickle
 
+import ase
+import tsase
+
 def pipein():
     return pickle.loads(''.join(sys.stdin.readlines()))
 
 def pipeout(a):
     print pickle.dumps(a, 0)
+
+def getpot(potstr):
+    try:
+        exec("pot = ase.calculators.%s()" % potstr)
+    except:
+        exec("pot = tsase.calculators.%s()" % potstr)
+    return pot
