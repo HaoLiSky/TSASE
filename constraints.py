baa24bc5a7836729ddac83bf84be9b7390eb7355
rye <rye@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2012-02-06 21:08:16 +0000

    block constraint that allows only translation of the block, but not rotation

--- constraints.py
+++ constraints.py
@@ -1,4 +1,5 @@
 from ase.constraints import FixBondLengths
+import numpy as np
 
 def FixBlock(indices_list):
     bonds = []
@@ -8,7 +9,26 @@ def FixBlock(indices_list):
                 bonds.append([indices[i], indices[j]])
     return FixBondLengths(bonds)
     
+class FixBlockAxes:
 
+    def __init__(self, index):
+        self.index = index
+        
+    def adjust_positions(self, oldpositions, newpositions):
+        step = newpositions[self.index] - oldpositions[self.index]
+        step = step.sum(0)/len(step)
+        newpositions[self.index] = oldpositions[self.index] + np.array([step])
+        
+    def adjust_forces(self, positions, forces):
+        forces[self.index] = forces[self.index].sum(0)/len(self.index)
+        
+    def copy(self):
+        return FixBlockAxes(self.index)
+        
+        
+        
+        
+    
 
 
     
