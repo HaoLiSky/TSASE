50a744686cc7dfb0f48f7078586e6b57432864e5
rye <rye@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2012-03-30 15:18:28 +0000

    refactored lisi potential and added tsase shell plugin for lisi

--- calculators/lisi/lisi.py
+++ calculators/lisi/lisi.py
@@ -3,9 +3,11 @@ import os
 
 from ase.calculators.lammps import LAMMPS
 
-def lisi(cmd = 'lmp_serial'):
-    os.environ['LAMMPS_COMMAND'] = cmd
+def lisi(cmd = None):
     cwd = os.getcwd()
+    if cmd == None:
+        cmd = os.path.join(cwd, 'lmp_serial')
+    os.environ['LAMMPS_COMMAND'] = cmd
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     parameters = {'pair_style': 'meam',
                   'pair_coeff': ['* * %s Li Si %s Li Si' % ('library.meam', 'LiSi.meam')]}
