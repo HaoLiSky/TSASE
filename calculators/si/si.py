65c8f39f2869f96766f57961a6316af2623071f8
rye <rye@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2012-05-01 20:13:01 +0000

    changed cutoff from default lammps to the one in the Cui paper, added a meam file for the pure Cui Si

--- calculators/si/si.py
+++ calculators/si/si.py
@@ -10,8 +10,8 @@ def si(cmd = None):
     os.environ['LAMMPS_COMMAND'] = cmd
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     parameters = {'pair_style': 'meam',
-                  'pair_coeff': ['* * library.meam Si NULL Si']}
-    files = ['library.meam']
+                  'pair_coeff': ['* * library.meam Si Si.meam Si']}
+    files = ['library.meam', 'Si.meam']
     calc = LAMMPS(parameters = parameters, files = files)
     os.chdir(cwd)
     return calc
