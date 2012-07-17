ec992ff88428ec0ea17351eb40cdb67cffa78d89
rye <rye@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2012-07-17 23:11:14 +0000

    restore concatenated poscar reading

--- io/__init__.py
+++ io/__init__.py
@@ -1,9 +1,27 @@
 import ase.io
+from ase.io import vasp as asevasp
 from con import read_con, write_con
 from feff import read_feff, write_feff
 from bopfox import read_bopfox, write_bopfox
 from vasp import read_xdatcar
 from socorro import read_socorro, write_socorro
+
+
+def read_vasps(filename):
+    f = open(filename, 'r')
+    data = []
+    while True:
+        try:
+            data.append(asevasp.read_vasp(f))
+        except:
+            f.close()
+            break
+    if len(data) < 1:
+        raise IOError, "Could not read file %s as vasp file." % filename
+    if len(data) < 2:
+        return data[0]
+    return data
+            
 
 def read(filename):
     try:
@@ -23,7 +41,7 @@ def read(filename):
     except:
         pass
     try:
-        return ase.io.read(filename+"@:", format='vasp')
+        return read_vasps(filename)
     except:
         pass
     try:
