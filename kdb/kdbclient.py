04f5b0970dc52649e1fe6e400a631bf57e96fb40
rye <rye@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2012-08-20 20:39:22 +0000

    all database settings configurable server side only

--- kdb/kdbclient.py
+++ kdb/kdbclient.py
@@ -5,6 +5,7 @@ import sys
 import httplib
 import urllib
 import json
+import shutil
 from optparse import OptionParser
 
 
@@ -14,9 +15,6 @@ def server_insert(args, options):
     params['saddle']   = ''.join(open(args[2], 'r').readlines())
     params['product']  = ''.join(open(args[3], 'r').readlines())
     params['mode']     = ''.join(open(args[4], 'r').readlines())
-    params['nf']  = options.nf
-    params['dc']  = options.dc
-    params['mac'] = options.mac
     params  = urllib.urlencode(params)
     headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/plain'}
     conn = httplib.HTTPConnection(host=options.host, port=options.port)
@@ -30,8 +28,6 @@ def server_insert(args, options):
 def server_query(args, options):
     params = {}
     params['reactant'] = ''.join(open(args[1], 'r').readlines())
-    params['nf']  = options.nf
-    params['dc']  = options.dc
     params  = urllib.urlencode(params)
     headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': 'text/plain'}
     conn = httplib.HTTPConnection(host=options.host, port=options.port)
@@ -56,15 +52,6 @@ if __name__ == "__main__":
         - or - 
        %prog query [options] reactant"""
     parser = OptionParser(usage = usage)
-    parser.add_option("-n", "--nf", dest = "nf", action="store", type="float", 
-                      help = "neighbor fudge parameter",
-                      default = 0.2)
-    parser.add_option("-c", "--dc", dest = "dc", action="store", type="float", 
-                      help = "distance cutoff parameter",
-                      default = 0.3)
-    parser.add_option("-m", "--mac", dest = "mac", action="store", type="float", 
-                      help = "mobile atom cutoff parameter",
-                      default = 0.7)
     parser.add_option("--host", dest = "host", help = "the hostname of a kdbserver",
                       default = "localhost")
     parser.add_option("--port", dest = "port", action="store", type="int", 
--- kdb/kdbserver.py
+++ kdb/kdbserver.py
@@ -5,11 +5,15 @@ import commands
 import tempfile
 import json
 import glob
+from optparse import OptionParser
 import bottle as b
+from kdb import MOBILE_ATOM_CUTOFF, NEIGHBOR_FUDGE, DISTANCE_CUTOFF
+
 
 QUERY_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'kdbquery.py')
 INSERT_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'kdbinsert.py')
 KDB_PATH = os.path.join(os.path.abspath(os.getcwd()), 'kdb')
+
 
 @b.post('/insert')
 def insert():
@@ -26,15 +30,12 @@ def insert():
     f = open(os.path.join(tmpdir, 'mode.dat'), 'w')
     f.write(b.request.forms.get('mode'))
     f.close()
-    nf = float(b.request.forms.get('nf'))
-    dc = float(b.request.forms.get('dc'))
-    mac = float(b.request.forms.get('mac'))
     output = commands.getoutput('%s %s %s %s %s --nf=%f --dc=%f --mac=%f --kdbdir=%s' % (INSERT_PATH,
                       os.path.join(tmpdir, 'reactant.con'),
                       os.path.join(tmpdir, 'saddle.con'),
                       os.path.join(tmpdir, 'product.con'),
                       os.path.join(tmpdir, 'mode.dat'),
-                      nf, dc, mac, KDB_PATH)) 
+                      NEIGHBOR_FUDGE, DISTANCE_CUTOFF, MOBILE_ATOM_CUTOFF, KDB_PATH)) 
     return output
     
 @b.post('/query')
@@ -43,10 +44,8 @@ def query():
     f = open(os.path.join(tmpdir, 'reactant.con'), 'w')
     f.write(b.request.forms.get('reactant'))
     f.close()
-    nf = float(b.request.forms.get('nf'))
-    dc = float(b.request.forms.get('dc'))
     output = commands.getoutput('%s %s --nf=%f --dc=%f --kdbdir=%s' % 
-                                (QUERY_PATH, os.path.join(tmpdir, 'reactant.con'), nf, dc, KDB_PATH)) 
+                                (QUERY_PATH, os.path.join(tmpdir, 'reactant.con'), NEIGHBOR_FUDGE, DISTANCE_CUTOFF, KDB_PATH)) 
     filelist = glob.glob(os.path.join('kdbmatches', '*'))
     ret = {}
     ret['stdout'] = output
@@ -58,6 +57,27 @@ def query():
 
 @b.route('/')
 def index():
-    return '<b>Hello  </b>'
+    return '<b>... under construction ...</b>'
 
-b.run(host='localhost', port=8080, reloader=True)
+if __name__ == "__main__":
+    # Parse command line options.
+    usage = """%prog [options]"""
+    parser = OptionParser(usage = usage)
+    parser.add_option("-n", "--nf", dest = "nf", action="store", type="float", 
+                      help = "neighbor fudge parameter",
+                      default = NEIGHBOR_FUDGE)
+    parser.add_option("-c", "--dc", dest = "dc", action="store", type="float", 
+                      help = "distance cutoff parameter",
+                      default = DISTANCE_CUTOFF)
+    parser.add_option("-m", "--mac", dest = "mac", action="store", type="float", 
+                      help = "mobile atom cutoff parameter",
+                      default = MOBILE_ATOM_CUTOFF)
+    parser.add_option("--host", dest = "host", help = "the hostname to use",
+                      default = "localhost")
+    parser.add_option("--port", dest = "port", action="store", type="int", 
+                      help = "the port to use", default = 8080)
+    options, args = parser.parse_args()
+    NEIGHBOR_FUDGE = options.nf
+    MOBILE_ATOM_CUTOFF = options.mac
+    DISTANCE_CUTOFF = options.dc
+    b.run(host=options.host, port=options.port, reloader=True)
