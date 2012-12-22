eb9482b243bd085b05224b51c7057bf699a7a784
xph <xph@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2012-12-22 23:24:23 +0000

    realtf on climbing images is corrected for ploting not-converged neb.dat

--- neb/ssneb.py
+++ neb/ssneb.py
@@ -185,7 +185,7 @@ class ssneb:
             # Timing box volume should have been done.
             self.path[i].totalf = numpy.vstack((self.path[i].f, self.path[i].st / self.jacobian))
             # realtf that needed by nebspline.pl is saved for output 
-            self.path[i].realtf = self.path[i].totalf
+            self.path[i].realtf = deepcopy(self.path[i].totalf)
             
             # If we're using the 'old' tangent, the tangent is defined as the
             # vector from the point behind the current image to the point in
