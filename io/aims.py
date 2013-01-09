2d39df750a0aeddf58ee081da7d9c8d2ef641dd5
chill <chill@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2013-01-09 17:26:33 +0000

    improved aims file reader to support lattice vectors and fractional coordinates

--- io/aims.py
+++ io/aims.py
@@ -6,14 +6,32 @@ def read_aims(filename):
 
     atoms = ase.Atoms()
 
+    l = 0
+    cell = numpy.zeros( (3,3) )
     for line in f:
         line = line[:line.find('#')]
         line = line.strip()
         if len(line) == 0: continue
         fields = line.split()
 
+
+        if fields[0] == 'lattice_vector':
+            cell[l,0] = float(fields[1])
+            cell[l,1] = float(fields[2])
+            cell[l,2] = float(fields[3])
+            l += 1
+            if l == 3:
+                atoms.set_cell(cell)
+                print cell
+
         if fields[0] == 'atom':
             pos = numpy.array((float(fields[1]),float(fields[2]),float(fields[3])))
+            element = fields[4]
+            atoms.append(ase.Atom(element, position=pos))
+
+        if fields[0] == 'atom_frac':
+            scaled_pos = numpy.array((float(fields[1]),float(fields[2]),float(fields[3])))
+            pos = numpy.dot(cell, scaled_pos)
             element = fields[4]
             atoms.append(ase.Atom(element, position=pos))
     f.close()
