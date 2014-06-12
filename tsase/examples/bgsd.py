abdc8a3f07ddc46207587c5a99249a9fcff6077a
jrduncan <jrduncan@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2014-06-12 15:38:36 +0000

    fix example

--- tsase/examples/bgsd.py
+++ tsase/examples/bgsd.py
@@ -1,24 +1,36 @@
 #!/usr/bin/env python
 
 import tsase
-import ase
 
 ######## load V #########################
 al = tsase.calculators.al()
 p = tsase.io.read_con('al.con')
 p.set_calculator(al)
-bmin = tsase.optimize.SDLBFGS(p)
-#########################################
 
-##### set calculator to be H landscape of the PES of p using BGSD_potential #######
+#### minimize p so the system is at a local minimum ##########
+bmin = tsase.optimize.SDLBFGS(p)
+bmin.run()
+
+##### set calculator to be H landscape using the PES of p and the BGSD_potential #######
 p1 = tsase.io.read_con('al.con')
 Hpotential = tsase.bgsd.BGSD_potential(pot = p,alpha = 5.0,beta = 0.2 ,dr=1.e-5)
 p1.set_calculator(Hpotential)
-###################################################################################
 
-####### run BGSD #########
+####### run BGSD ##########################
 bgsd = tsase.bgsd.BGSD(p,alpha=5.0,beta=0.27,displace_atomlist=[25,26,27],displace_radius=0.0)
+
+"""
+
+The function 'find saddle' samples the beta isosurface, minimizes H and the gradient squared landscape, 
+and checks to see if a 1st order saddle point connected to the reactant state is found.  
+It returns two parameters. The first parameter is True if a first order connected saddle point is found and 
+False otherwise.  The second parameters is the number force calls required. 
+The script produces a file of called 'SP.con' which is the critical point found in the optimization.  
+
+"""
+
 converged, ForceCalls = bgsd.find_saddle()
 
+###########################################
 print 'Force Calls:',ForceCalls
 print 'Was first order saddle point found',converged
