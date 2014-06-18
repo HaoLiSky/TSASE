3364c722177cf1d5cdcd0b991539bd51098e2878
stauff <stauff@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2014-06-18 18:49:57 +0000

    modified theme

--- docs/conf.py
+++ docs/conf.py
@@ -119,12 +119,12 @@ html_short_title = "TSASE"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo =  "_static/tsase_greenoutline.png"
+html_logo =  "_static/tsaselogo_orange.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon =  "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
