0b4406b9e97df909d735b383e3f2946c8aaf578d
rb36955 <rb36955@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2016-08-10 15:35:52 +0000

    basin hopping import

--- tsase/optimize/__init__.py
+++ tsase/optimize/__init__.py
@@ -4,3 +4,4 @@ from tsase.optimize.sdlbfgs import SDLBFGS
 from tsase.optimize.mdmin import MDMin
 from tsase.optimize.basin import BasinHopping
 #from tsase.optimize.basin_adjust import BasinHopping2
+from tsase.optimize.minimahopping import MinimaHopping
