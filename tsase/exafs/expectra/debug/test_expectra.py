e2def5d5e7667622fac77c6c04171c77fecaccbe
leili <leili@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2016-09-09 07:10:19 +0000

    test_expectra.py use cal_exafs.py as calculator to get EXAFS and deviation

--- tsase/exafs/expectra/debug/test_expectra.py
+++ tsase/exafs/expectra/debug/test_expectra.py
@@ -2,14 +2,17 @@
 
 from expectra.cal_exafs import Expectra
 from expectra.io import read_xdatcar, read_con, read_chi
-from ase import atoms
+#from ase import atoms
 from ase.io.vasp import read_vasp
 
 def main():
-    trajectory = read_vasp(filename='CONTCAR')
-    calc = Expectra(atoms=trajectory, kmax = 10.0)
-    trajectory.set_calculator(calc)
-    chi_devia = trajectory.get_potential_energy()
+    filename='CONTCAR'
+    p1 = read_xdatcar(filename)
+#    print(p1[0].get_positions())
+#    print(type(p1[0]))
+    calc = Expectra(atoms=p1[0], kmax = 10.0)
+    p1[0].set_calculator(calc)
+    chi_devia = p1[0].get_potential_energy()
     print(chi_devia)
 if __name__ == '__main__':
     main()
