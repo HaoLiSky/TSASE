26a1796b3f1e18adc6176aedbb5f697fbdbf8b6a
leili <leili@c7672663-de42-4e7d-b2dc-0a033dba07a0>
2016-09-09 07:09:14 +0000

    cal_exafs calculator has been debugged. More functions (i.e. mpirun and error detection) will be added

--- tsase/exafs/expectra/expectra/cal_exafs.py
+++ tsase/exafs/expectra/expectra/cal_exafs.py
@@ -8,11 +8,11 @@ with mpi_import():
     import argparse
     import sys
 
-
     from expectra.exafs import exafs_first_shell, exafs_multiple_scattering
-    from expectra.io import read_xdatcar, read_con
-    from ase.io import read
+    from expectra.io import read_xdatcar, read_con, read_chi
     from ase.calculators.calculator import Calculator, all_changes, Parameters
+    from expectra.aselite import Atoms
+    from expectra.feff import load_chi_dat
 
 COMM_WORLD = mpi4py.MPI.COMM_WORLD
 
@@ -23,10 +23,10 @@ def mpiexcepthook(type, value, traceback):
 sys.excepthook = mpiexcepthook
 
 #need to modify it to save a history of (chi, k)
-def save_result(k, chi):
+def save_result(k, chi, filename):
     if COMM_WORLD.rank != 0: return
     print 'saving result to chi.dat'
-    f = open('chi.dat', 'w')
+    f = open(filename, 'w')
     for i in xrange(len(k)):
         f.write("%6.3f %16.8e\n" % (k[i], chi[i]))
     f.close()
@@ -48,6 +48,8 @@ def get_default_absorber(atoms, args):
         sys.exit(2)
 
 def exafs_trajectory(args, trajectory):
+#    print "trajectory: "
+#    print(type(trajectory))
     if args.multiple_scattering:
         k, chi = exafs_multiple_scattering(args.S02, args.energy_shift, 
                 args.absorber, args.ignore_elements, args.edge, args.rmax, 
@@ -62,9 +64,9 @@ def exafs_trajectory(args, trajectory):
 
 #calculate the deviation of theoretical EXAFS from experimental EXAFS
 def calc_deviation(chi_exp,chi_theory):
-    chi_devi = 0
-    for i in range(0, len(chi_exp)):
-        chi_devi = chi_devi + (chi_exp - chi_theory)**2
+    chi_exp_array = numpy.asarray(chi_exp)
+    chi_thry_array = numpy.asarray(chi_theory)
+    chi_devi = numpy.sum(numpy.square(chi_exp_array - chi_thry_array))
 
     return chi_devi/len(chi_exp)
 
@@ -84,7 +86,7 @@ def rescale_chi_calc(k_std, chi_src, k_src, kmax):
 #      except MyValidationError as exception:
 #          print exception.message
     i = 0   
-    while ( 0 <= i < len(k_std) and k_std[i] <= kmax):
+    while ( 0 <= i < len(k_std) and k_std[i] < kmax):
         for j in range(1,len(k_src)):
             if k_src[j-1] < k_std[i] and k_std[i] < k_src[j]:
                 chi_temp.append(numpy.interp(k_std[i],
@@ -106,13 +108,16 @@ class Expectra(Calculator):
     implemented_properties = ['chi_deviation']
 
     default_parameters = dict(
+        multiple_scattering = True,
+        ignore_elements = None,
         neighbor_cutoff = 6.0,
-        s02 = 0.89,
+        rmax = 6.0,
+        S02 = 0.89,
         energy_shift = 3.4,
         edge = 'L3',
         absorber = 'Au',
         skip = 0,
-        exp_chi_fie = 'chi_exp.dat',
+        exp_chi_file = 'chi_exp.dat',
         output_file = 'chi.dat')
 
     def __int__(self, label='EXAFS', 
@@ -144,15 +149,23 @@ class Expectra(Calculator):
         changed_parameters = Calculator.set(self, **kwargs)
         if changed_parameters:
            self.reset()
+
     def get_potential_energy(self, atoms=None, force_consistent=False):
+        print(type(atoms))
         self.calculate(atoms, 'chi_deviation')
         return self.chi_deviation
 
     def calculate(self, atoms=None, properties=None):
 
         parameters = self.parameters
-        trajectory = COMM_WORLD.bcast(self.atoms)
-        self.absorber = get_default_absorber(trajectory, parameters)
+        print "atoms: " 
+        print(type(atoms))
+#        print(atoms.get_positions())
+        trajectory = []
+        trajectory.append(atoms)
+#        print(type(trajectory))
+        trajectory = COMM_WORLD.bcast(trajectory)
+#        self.absorber = get_default_absorber(trajectory, parameters)
         k, chi = exafs_trajectory(parameters, trajectory)
 
         #load experimental chi data
@@ -162,9 +175,13 @@ class Expectra(Calculator):
             k_exp, chi_exp = load_chi_dat(parameters.exp_chi_file)
 
         #interpolate chi_exp values based on k values provided in calculated data
-        k_exp, chi_exp = rescale_chi_calc(k, chi_exp, k_exp, self.kmax)
+        k_exp, chi_exp = rescale_chi_calc(k, chi_exp, k_exp, parameters.kmax)
+        k, chi = rescale_chi_calc(k, chi, k, parameters.kmax)
 
-        save_result(k, chi)
+        filename1 = 'chi.dat'
+        filename2 = 'rescaled_exp_chi.dat'
+        save_result(k, chi, filename1)
+        save_result(k_exp, chi_exp, filename2)
     
         self.chi_deviation = calc_deviation(chi_exp, chi)
 
